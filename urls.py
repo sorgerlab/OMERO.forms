6cf010119de5860bda3713e57730292e1201430c
dpwrussell <douglas_russell@hms.harvard.edu>
2016-11-08 14:10:35 -0500

    Remove 'omeroforms' part of urls

--- urls.py
+++ urls.py
@@ -7,72 +7,72 @@ urlpatterns = patterns(
     'django.views.generic.simple',
 
     # Designer App
-    url(r'^omeroforms/designer',
+    url(r'^designer',
         views.designer,
         name='omeroforms_designer'),
 
     # API
-    url(r'^omeroforms/$',
+    url(r'^$',
         lambda x: None,
         name='omeroforms_base'),
 
     # List all forms
-    url(r'^omeroforms/list_forms/$',
+    url(r'^list_forms/$',
         views.list_forms,
         name='omeroforms_list_forms'),
 
     # List the forms that are assigned to the user's active group that
     # apply to the object type
-    url(r'^omeroforms/list_applicable_forms/(?P<obj_type>\w+)/$',
+    url(r'^list_applicable_forms/(?P<obj_type>\w+)/$',
         views.list_applicable_forms,
         name='omeroforms_list_applicable_forms'),
 
     # Get a form (latest version)
-    url(r'^omeroforms/get_form/(?P<form_id>\w+)/$',
+    url(r'^get_form/(?P<form_id>\w+)/$',
         views.get_form,
         name='omeroforms_get_form'),
 
     # Get data for a form (latest version) for a certain object
-    url(r'^omeroforms/get_form_data/'
+    url(r'^get_form_data/'
         r'(?P<form_id>\w+)/(?P<obj_type>\w+)/(?P<obj_id>\w+)/$',
         views.get_form_data,
         name='omeroforms_get_form_data'),
 
     # Get assignments (restricted to those the user can unassign)
-    url(r'omeroforms/get_form_assignments/$',
+    url(r'get_form_assignments/$',
         views.get_form_assignments,
         name='omeroforms_get_form_assignments'),
 
     # Get the entire history of a form including all data and the forms used
     # to enter that data
-    url(r'^omeroforms/get_form_data_history/'
+    url(r'^get_form_data_history/'
         r'(?P<form_id>\w+)/(?P<obj_type>\w+)/(?P<obj_id>\w+)/$',
         views.get_form_data_history,
         name='omeroforms_get_form_data_history'),
 
     # Get groups that the user can manage
-    url(r'^omeroforms/get_managed_groups/$',
+    url(r'^get_managed_groups/$',
         views.get_managed_groups,
         name='omeroforms_get_managed_groups'),
 
     # Lookup usernames by uid
-    url(r'^omeroforms/get_users/$',
+    url(r'^get_users/$',
         views.get_users,
         name='omeroforms_get_users'),
 
     # Save a form version (potentially a new form)
-    url(r'^omeroforms/save_form/$',
+    url(r'^save_form/$',
         views.save_form,
         name='omeroforms_save_form'),
 
     # Save data for a form
-    url(r'^omeroforms/save_form_data/'
+    url(r'^save_form_data/'
         r'(?P<form_id>\w+)/(?P<obj_type>\w+)/(?P<obj_id>\w+)/$',
         views.save_form_data,
         name='omeroforms_save_form_data'),
 
     # Save a form assignment
-    url(r'^omeroforms/save_form_assignment/$',
+    url(r'^save_form_assignment/$',
         views.save_form_assignment,
         name='omeroforms_save_form_assignment'),
 )
